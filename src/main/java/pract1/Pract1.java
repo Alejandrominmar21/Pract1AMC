dceb952c09ef569b9ef5c705c53f30d0343525d3
Alejandro <alejandro.minchon@alu.uhu.es>
2025-10-16 19:46:47 +0200

    prueba

--- src/main/java/pract1/Pract1.java
+++ src/main/java/pract1/Pract1.java
@@ -19,7 +19,7 @@ public class Pract1 {
         
         ArrayList<Punto> dataset = new ArrayList<>();
         dataset.add(new Punto(0,0));
-        dataset.add(new Punto(2,3));
+        dataset.add(new Punto(3,3));
         dataset.add(new Punto(4,4));
         dataset.add(new Punto(5,5));
         
